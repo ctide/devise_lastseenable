741db5747f8e13115121a22b265183851b362307
Shumilkin Gena <shumilkin.gena@yandex.ru>
2015-06-02 16:47:35 +0300

    Update model.rb

--- lib/devise_lastseenable/model.rb
+++ lib/devise_lastseenable/model.rb
@@ -4,9 +4,8 @@ module Devise
   module Models
     module Lastseenable
       def stamp!
-        if self.last_seen.to_i < (Time.now - 5.minutes).to_i
-          self.last_seen = DateTime.now
-          self.save!
+        if self.last_seen < 5.minutes.ago
+          self.update_attribute(:last_seen, DateTime.now)
         end
       end
     end
